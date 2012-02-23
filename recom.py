4aafbe87270fa743b2454f9fd6b67f64b90c93d3
Abdullah Shams Bin Tariq <asbtariq@ru.ac.bd>
2012-02-23 14:16:07 +0100

    2-norm, Pearson and Rogers-Tanomoto implemented

--- recom.py
+++ recom.py
@@ -1,7 +1,6 @@
 import inputdata
 
 alldata = inputdata.raw_scores
-print alldata
 
 readers = []
 for ir in alldata.iterkeys(): 
@@ -19,40 +18,51 @@ papers = list(set(papers))
 
 import numpy
 scores = numpy.zeros(shape=(len(readers),len(papers)))
-print scores
+binscores = numpy.zeros(shape=(len(readers),len(papers)))
 
 for i in range(len(readers)):
     for j in range(len(papers)):
         if alldata[readers[i]].has_key(papers[j]):
             scores[i,j]=alldata[readers[i]][papers[j]]
+            binscores[i,j]=1
+print
+print 'Scores matrix'
+print scores
+print
+print 'Binary scores matrix'
+print binscores
 
-'''
+from math import sqrt
+def my2norm(vec1,vec2):
+    if (len(vec1)!=len(vec2)): 
+        print 'error: unequal vector sizes'
+    norm = 0
+    for i in range(len(vec1)):
+        if(vec1[i]!=0 and vec2[i]!=0):
+            norm += pow((vec1[i]-vec2[i]),2)
+    return sqrt(norm)
+
+from scipy import stats
+from scipy import spatial
+
+dissimIndex = numpy.zeros(shape=(len(readers),len(readers)))
+correlPearson = numpy.zeros(shape=(len(readers),len(readers)))
+distRogTani = numpy.zeros(shape=(len(readers),len(readers)))
 for i in range(len(readers)):
-    score_vector[i] = alldata[readers[i]]
+    bool
+    for j in range(len(readers)):
+        dissimIndex[i][j] = my2norm(scores[i],scores[j])
+        correlPearson[i][j] = stats.pearsonr(scores[i],scores[j])[0]
+        distRogTani[i][j] = spatial.distance.rogerstanimoto(binscores[i],binscores[j])
+print
+print "Reader's list: ",readers
+print
+print "Dissimilarity index matrix"
+print dissimIndex     
+print
+print "Pearson correlation matrix"
+print correlPearson     
+print
+print "Rogers-Tanimoto distance matrix"
+print distRogTani     
 
-def two-norm(vec1,vec2)
-'''
-'''
-print alldata['Mehrdad Mapping']['Chen 2002']
-print int(alldata['Mehrdad Mapping'].has_key('Chen 2002'))
-print int(alldata['Mehrdad Mapping'].has_key('Jackson 1999'))
-print alldata['Mehrdad Mapping']['Jackson 1999']
-'''
-
-
-
-
-'''
-infile = open('inputdata','r')
-data = infile.readlines()
-print data
-
-datastr = open('inputdata').read()
-eval(datastr)
-print datastr
-datastr1 = datastr.replace('\n','')
-datastr2 = datastr1.lstrip('raw scores = ')
-print datastr2
-data = eval(datastr2)
-print data
-'''    
